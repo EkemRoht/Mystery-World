fe703e849c02413e4ee8d1639f7a7fd1dedf910c
EkemRoht <ekemroht@gmail.com>
2023-12-17 16:05:42 +0500

    world history evolution (start)

---
+++ src/lib/WPComponents/WorldEvolution.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {world} from "../js/worldStore.js";
+
+    let logs = [];
+    function evolution () {
+        let arr = [];
+        $world.regions.forEach(region => {
+            region.locations.forEach(location => {
+                let msg = `${location.name} from ${region.name}`;
+                arr.push(msg);
+            });
+        });
+        logs = arr;
+    }
+</script>
+
+{#each logs as post}
+    <blockquote>
+        {post}
+    </blockquote>
+{/each}
+<button on:click={evolution}>Continue</button>
--- src/lib/WPComponents/WorldProto.svelte
+++ src/lib/WPComponents/WorldProto.svelte
@@ -1,12 +1,14 @@
 <script>
     import World from "../js/world.js";
     import RegionItem from "./RegionItem.svelte";
+    import WorldEvolution from "./WorldEvolution.svelte";
     import {world} from "../js/worldStore.js";
     $world = new World(6, 3);
 </script>
 
 <article>
     <header>Generation Logs</header>
+    <WorldEvolution/>
 </article>
 <details>
     <summary>Regions List</summary>
